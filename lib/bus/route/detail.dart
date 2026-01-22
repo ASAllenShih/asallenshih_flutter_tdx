1ff115895975e7dc2c1e46a1a1a01062443fa199
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2026-01-22 18:25:07 +0800

    新增 TdxBusRouteDetail 類別及其功能，包含顯示公車路線詳細資訊的彈出視窗和地圖顯示功能

---
+++ lib/bus/route/detail.dart
@@ -0,0 +1,177 @@
+import 'package:asallenshih_flutter_tdx/asallenshih_flutter_tdx.dart';
+import 'package:asallenshih_flutter_tdx/type/basic/city.dart';
+import 'package:asallenshih_flutter_tdx/type/bus/route.dart';
+import 'package:asallenshih_flutter_util/device.dart';
+import 'package:asallenshih_flutter_util/log.dart';
+import 'package:asallenshih_flutter_util/webview.dart' deferred as wv;
+import 'package:cross_platform_ui/cross_platform.dart';
+import 'package:flutter/material.dart' hide Route;
+
+class TdxBusRouteDetail {
+  final City city;
+  final Route route;
+  final String closeText;
+  final String? loadingText;
+  final String? operatorText;
+  final String? departureText;
+  final String? destinationText;
+  final String? ticketPriceText;
+  final String? viewRouteMapText;
+  Route? routeDetail;
+  TdxBusRouteDetail(
+    this.city,
+    this.route, {
+    required this.closeText,
+    this.loadingText,
+    this.operatorText,
+    this.departureText,
+    this.destinationText,
+    this.ticketPriceText,
+    this.viewRouteMapText,
+  });
+  Future<void> popup(BuildContext context) async {
+    if (routeDetail == null) {
+      dialogShow(
+        context: context,
+        builder: (context) {
+          return CrossPlatformDialogAlert(
+            title: Text(loadingText ?? ''),
+            content: CrossPlatformProgressIndicator(),
+          ).widget;
+        },
+      );
+      await TdxBusRouteApi.getPopup(city, route);
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
+    }
+    final String routeName = route.routeName?.text ?? '';
+    final String? operator = routeDetail?.operators
+        ?.map((o) => o.operatorName?.text ?? '')
+        .join(', ');
+    final String? departureStop = routeDetail?.departureStopName?.text;
+    final String? destinationStop = routeDetail?.destinationStopName?.text;
+    final String? ticketPrice = routeDetail?.ticketPriceDescription?.text;
+    await dialogShow(
+      context: context,
+      builder: (context) {
+        return CrossPlatformDialogAlert(
+          icon: const Icon(Icons.directions_bus),
+          title: Text(routeName),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (operator != null && operatorText != null)
+                Text('$operatorText: $operator'),
+              if (departureStop != null && departureText != null)
+                Text('$departureText: $departureStop'),
+              if (destinationStop != null && destinationText != null)
+                Text('$destinationText: $destinationStop'),
+              if (ticketPrice != null && ticketPriceText != null)
+                Text('$ticketPriceText: $ticketPrice'),
+            ],
+          ),
+          actions: [
+            if (routeDetail?.routeMapImageUrl != null &&
+                viewRouteMapText != null &&
+                Device.supportedWebViewOrIframe)
+              CrossPlatformButtonDialog(
+                child: Text(viewRouteMapText!),
+                onPressed: () {},
+              ),
+            CrossPlatformButtonDialog(
+              child: Text(closeText),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              isDefaultAction: true,
+            ),
+          ],
+        ).widget;
+      },
+    );
+  }
+
+  Future<void> map(BuildContext context) async {
+    final String routeName = route.routeName?.text ?? '';
+    final String? mapUrl = routeDetail?.routeMapImageUrl;
+    if (mapUrl == null || mapUrl.isEmpty) {
+      return;
+    }
+    await dialogShow(
+      context: context,
+      builder: (context) {
+        return CrossPlatformDialogAlert(
+          icon: const Icon(Icons.directions_bus),
+          title: Text(routeName),
+          content: SizedBox(
+            width: MediaQuery.of(
+              context,
+            ).removePadding(removeLeft: true, removeRight: true).size.width,
+            child: _map(mapUrl: _mapUrl(mapUrl)),
+          ),
+          actions: [
+            CrossPlatformButtonDialog(
+              child: Text(closeText),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              isDefaultAction: true,
+            ),
+          ],
+        ).widget;
+      },
+    );
+  }
+
+  static String _mapUrl(String mapUrl) => mapUrl.startsWith('https://')
+      ? mapUrl
+      : mapUrl.startsWith('http://')
+      ? 'https://${mapUrl.substring(7)}'
+      : 'https://$mapUrl';
+  static Widget _map({required String mapUrl}) {
+    if (mapUrl.endsWith('.png') ||
+        mapUrl.endsWith('.jpg') ||
+        mapUrl.endsWith('.jpeg')) {
+      return Image.network(
+        _mapUrl(mapUrl),
+        webHtmlElementStrategy: WebHtmlElementStrategy.fallback,
+        errorBuilder:
+            (BuildContext context, Object error, StackTrace? stackTrace) {
+              log.e('Image load error: $error');
+              return _webviewWidget(mapUrl: _mapUrl(mapUrl));
+            },
+      );
+    }
+    return _webviewWidget(mapUrl: _mapUrl(mapUrl));
+  }
+
+  static Widget _webviewWidget({required String mapUrl}) => FutureBuilder(
+    future: _webview(mapUrl: mapUrl),
+    builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
+      if (snapshot.connectionState == ConnectionState.done &&
+          snapshot.hasData) {
+        return snapshot.data!;
+      }
+      return Container();
+    },
+  );
+
+  static Future<Widget> _webview({required String mapUrl}) async {
+    await wv.loadLibrary();
+    final webview = wv.Webview();
+    final webViewSettings = wv.Webview.settings();
+    final webViewEnvironmentFuture = wv.Webview.environment();
+    final webViewEnvironment = webViewEnvironmentFuture is Future<dynamic>
+        ? await webViewEnvironmentFuture
+        : webViewEnvironmentFuture;
+    return webview.widget(
+          webViewEnvironment: webViewEnvironment,
+          webViewSettings: webViewSettings,
+          initialUri: Uri.tryParse(mapUrl),
+        ) ??
+        Container();
+  }
+}
